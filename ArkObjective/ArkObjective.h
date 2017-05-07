77e972a2e6f127f01a1d86bae135f44273f20f29
1vanov <capnweb@yandex.ru>
2017-05-07 17:49:56 +0300

    Initial Commit

---
+++ ArkObjective/ArkObjective.h
@@ -0,0 +1,19 @@
+//
+//  ArkObjective.h
+//  ArkObjective
+//
+//  Created by Ilya on 07.05.17.
+//  Copyright © 2017 Elias J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArkObjective.
+FOUNDATION_EXPORT double ArkObjectiveVersionNumber;
+
+//! Project version string for ArkObjective.
+FOUNDATION_EXPORT const unsigned char ArkObjectiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArkObjective/PublicHeader.h>
+
+
